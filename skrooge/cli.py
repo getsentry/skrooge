04ed7b2a373abd6194cfb585624f9076982063a7
Michael Warkentin <michael@sentry.io>
2023-12-20 12:29:13 -0500

    add skeleton of skrooge estimate cmd

--- skrooge/cli.py
+++ skrooge/cli.py
@@ -7,15 +7,36 @@ def cli():
     "A quick and dirty kubernetes cost estimator"
 
 
-@cli.command(name="command")
-@click.argument(
-    "example"
+@cli.command(name="estimate")
+@click.option(
+    "-r",
+    "--replicas",
+    help="The number of replicas in the deployment",
+    type=click.INT,
 )
 @click.option(
-    "-o",
-    "--option",
-    help="An example option",
+    "-c",
+    "--cpu",
+    help="The amount of CPU change",
+    type=click.INT,
 )
-def first_command(example, option):
-    "Command description goes here"
-    click.echo("Here is some output")
+@click.option(
+    "-m",
+    "--mem",
+    help="The amount of memory change in MiB",
+    type=click.INT,
+)
+@click.option(
+    "-i",
+    "--instance",
+    help="The instance type this deployment is running on",
+)
+def estimate(replicas, cpu, mem, instance):
+    "Quick estimate of the cost or savings a kubernetes scale update will incur"
+    click.echo(f"{replicas=}")
+    click.echo(f"{cpu=}")
+    click.echo(f"{mem=}")
+    click.echo(f"{instance=}")
+
+if __name__ == '__main__':
+    cli()
